55aab5214038a64a95afca1906d9961946c18ceb
Matheus <connect.malves@gmail.com>
2019-10-03 17:05:21 -0300

    Adding default api_token

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'api_token' => Str::random(60)
+        'api_token' => 'kxBDR7b01xC4VNg2o97F5SSs4XCcJyn9y6dxkaYkwF5odjeL9OpQDAMjW7cS'
     ];
 });
