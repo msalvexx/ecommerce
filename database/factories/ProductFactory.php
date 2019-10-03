d2def88916a5629b746afc8de6ce41a536aa6ea3
Matheus <connect.malves@gmail.com>
2019-10-03 15:51:59 -0300

    Adding Factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,11 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Product;
+use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name'              => $faker->word,
+        'product_type_id'   => $faker->numberBetween(1, 10),
+        'brand'  => $faker->word,
+        'amount' => $faker->randomFloat(2, 1, 2000),
+        'stock'  => $faker->randomNumber(3)
     ];
 });
